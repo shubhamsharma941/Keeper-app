cec47c2b45c9299ab49ecacd180fa047827b4730
Shubham Sharma <shubhamsharmas941@gmail.com>
2021-10-03 20:27:28 +0530

    Updates

---
+++ precache-manifest.36490be71d8c24ddcc52da847865d533.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c44e0268fb0d6c840af969c6f4fa32",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "cf47a55fc0e744059825",
+    "url": "/Keeper-app/static/js/2.c9c50ad6.chunk.js"
+  },
+  {
+    "revision": "87359069af9f67bd94a5",
+    "url": "/Keeper-app/static/js/main.b1229117.chunk.js"
+  },
+  {
+    "revision": "dd1051f0b09b1315230e",
+    "url": "/Keeper-app/static/js/runtime-main.19c413c2.js"
+  }
+]);
